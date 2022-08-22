26aa99c48d5ebdaf8e70d83960992512f74377e2
Tita <tita.enstad@gmail.com>
2022-08-22 13:28:10 +0200

    Remove old radar

--- build-your-own-radar/src/site.js
+++
@@ -1,8 +0,0 @@
-require('./common')
-require('./images/logo.png')
-require('./images/radar_legend.png')
-require('./gtm.js')
-
-const GoogleSheetInput = require('./util/factory')
-
-GoogleSheetInput().build()
