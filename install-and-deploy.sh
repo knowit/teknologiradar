09b88cba922ddc473d6a787f29a4e1a3d4ed0c03
Elg <klink100@gmail.com>
2022-08-24 14:06:25 +0200

    Create install and deploy script

---
+++ install-and-deploy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+data_folder="data"
+frontend_folder="frontend"
+frontend_cdk_folder="frontend-cdk"
+radar_folder="$frontend_folder/radar"
+
+root_folder="$(pwd)"
+
+fatal() {
+    echo "⛔️$*" >&2
+    exit 1
+}
+
+#arg 1: the command to run, the exit code will be checked
+#arg 2: the error message to display if the command failes to return exitcode 0
+check_dependency () {
+    err_msg="$2"
+    test_cmd="$1"
+    # echo "Running test command '$test_cmd'"
+    eval "$test_cmd 1>/dev/null 2>1"
+    exit_code=$?
+    if [[ ! "$exit_code" -eq "0" ]]; then
+        echo "⛔️ cmd executed: $test_cmd" >&2
+        fatal $err_msg
+    fi
+}
+
+echo "🤖 Check whether dependencies are installed"
+check_dependency "python3 -c \"import pandas\"" "🤒 Either python3 or the pandas module is not installed" 
+check_dependency "cdk --version" "🤕 Failed to find AWS cdk" 
+check_dependency "npm --version" "🤧 Failed to find npm" 
+
+echo "🤩 All dependencies found! "
+
+echo "🪚 Generating the radar files"
+for file in "${data_folder}"/*.csv; do
+    base_filename="${file##*/}"
+    echo "🪵 Generating radar files from $base_filename"
+    python3 csv_to_md.py "$file" "$radar_folder/$base_filename" || fatal "😵‍💫 Failed to generate markdown files from $file to $radar_folder/$base_filename"
+done
+echo "🛖 Generated radar markdown files successfully"
+
+# Build the website
+
+echo "🧰 Building the website"
+
+cd $frontend_folder || fatal "Failed to find the frontend folder!"
+
+echo "🔧 First time setup of the project"
+npm install || fatal "Failed to run npm install"
+echo "🔨 Compiling files"
+npm run build || fatal "Failed to build frontend"
+echo "🔩 Generating json files files"
+./dist_scripts/scripts/generateJson.js || fatal "Failed to generate the rd.json file"
+echo "🛠 Creating static files"
+./dist_scripts/scripts/createStaticFiles.js || fatal "failed to create the static files"
+ 
+echo "🛌 Website built"
+
+echo "📤 Deploying website"
+cd "$root_folder/$frontend_cdk_folder" || fatal "Failed to find the frontend cdk folder"
+
+echo "📝 Displaying infrastructure difference built"
+cdk diff || fatal "Failed to diff infrastructure changes"
+echo "📈 Deploying changes to aws"
+cdk deploy || fatal "Failed to deploy to aws"
+
+echo "💫🏋🏿🧙🏿‍♂️ Done!"
