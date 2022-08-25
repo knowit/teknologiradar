d80e852e89493aed244805f3bf673dc3a483f48b
Elg <klink100@gmail.com>
2022-08-25 13:05:24 +0200

    Add suggestion for missing dependencies

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ source ./scripts/common.sh
 
 echo "🤖 Check whether dependencies are installed"
 check_dependency "test $(python3 --version | perl -pe 'if(($_)=/((?<=Python 3\.)[0-9]+)/){$_.="\n"}') -ge $min_python3_minor_version" "🚧Python 3.$min_python3_minor_version+ is required, you have $(python3 --version)"
-check_dependency "python3 -c \"import pandas\"" "🤒 Failed to find the pandas module for python"
-check_dependency "npm --version" "🤧 Failed to find npm"
-check_dependency "node --version" "😳 Failed to find node"
+check_dependency "python3 -c \"import pandas\"" "🤒 Failed to find the pandas module for python. run: python3 -m pip install pandas"
+check_dependency "npm --version" "🤧 Failed to find npm. Suggestion: use nvm https://github.com/nvm-sh/nvm"
+check_dependency "node --version" "😳 Failed to find node. Suggestion: use nvm https://github.com/nvm-sh/nvm"
 
 echo "🤩 All dependencies found! "
 
