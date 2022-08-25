c0c31eeff24ea690d285dddf10019aa9e065a643
Elg <klink100@gmail.com>
2022-08-25 11:08:53 +0200

    Move setup to its own script file

--- install-and-deploy.sh
+++ install-and-deploy.sh
@@ -1,66 +1,15 @@
 #!/bin/bash
 
 start_folder="$(pwd)"
-data_folder="data"
-frontend_folder="frontend"
 frontend_cdk_folder="frontend-cdk"
-radar_folder="$frontend_folder/radar"
-min_python3_minor_version="8"
 
-fatal() {
-    echo "⛔️$*" >&2
-    exit 1
-}
+source ./scripts/common.sh
 
-#arg 1: the command to run, the exit code will be checked
-#arg 2: the error message to display if the command failes to return exitcode 0
-check_dependency () {
-    err_msg="$2"
-    test_cmd="$1"
-    # echo "Running test command '$test_cmd'"
-    eval "$test_cmd 1>/dev/null 2>&1"
-    exit_code=$?
-    if [[ ! "$exit_code" -eq "0" ]]; then
-        echo "⛔️cmd executed: $test_cmd" >&2
-        fatal "$err_msg"
-    fi
-}
-
-echo "🤖 Check whether dependencies are installed"
-check_dependency "test $(python3 --version | perl -pe 'if(($_)=/((?<=Python 3\.)[0-9]+)/){$_.="\n"}') -ge $min_python3_minor_version" "🚧Python 3.$min_python3_minor_version+ is required, you have $(python3 --version)"
-check_dependency "python3 -c \"import pandas\"" "🤒 Failed to find the pandas module for python"
-check_dependency "cdk --version" "🤕 Failed to find AWS cdk" 
-check_dependency "npm --version" "🤧 Failed to find npm"
-check_dependency "node --version" "😳 Failed to find node"
+echo "👾 Checking if you are logged into aws"
 check_dependency "aws sts get-caller-identity > /dev/null" "🕵️You are not logged into the aws cli"
+echo "🤑 You're logged in!"
 
-echo "🤩 All dependencies found! "
-
-echo "🪚 Generating the radar files"
-for file in "${data_folder}"/*.csv; do
-    base_filename="${file##*/}"
-    revision_folder_name="${base_filename%.csv}"
-    echo "🪵 Generating radar files from $base_filename"
-    python3 csv_to_md.py "$file" "$radar_folder/$revision_folder_name" || fatal "😵‍💫 Failed to generate markdown files from $file to $radar_folder/$base_filename."
-done
-echo "🛖 Generated radar markdown files successfully"
-
-# Build the website
-
-echo "🧰 Building the website"
-
-cd $frontend_folder || fatal "Failed to find the frontend folder!"
-
-echo "🔧 First time setup of website frontend"
-npm install || fatal "Failed to run npm install for frontend"
-echo "🔨 Compiling files"
-npm run build || fatal "Failed to build frontend"
-echo "🔩 Generating json files files"
-node ./dist_scripts/scripts/generateJson.js || fatal "Failed to generate the rd.json file"
-echo "🛠 Creating static files"
-node ./dist_scripts/scripts/createStaticFiles.js || fatal "failed to create the static files"
- 
-echo "🛌 Website built"
+bash ./setup.sh || fatal "Failed to setup repository"
 
 echo "📤 Deploying website"
 cd "$start_folder/$frontend_cdk_folder" || fatal "Failed to find the frontend cdk folder"
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+data_folder="data"
+frontend_folder="frontend"
+radar_folder="$frontend_folder/radar"
+min_python3_minor_version="8"
+
+source ./scripts/common.sh
+
+echo "🤖 Check whether dependencies are installed"
+check_dependency "test $(python3 --version | perl -pe 'if(($_)=/((?<=Python 3\.)[0-9]+)/){$_.="\n"}') -ge $min_python3_minor_version" "🚧Python 3.$min_python3_minor_version+ is required, you have $(python3 --version)"
+check_dependency "python3 -c \"import pandas\"" "🤒 Failed to find the pandas module for python"
+check_dependency "cdk --version" "🤕 Failed to find AWS cdk" 
+check_dependency "npm --version" "🤧 Failed to find npm"
+check_dependency "node --version" "😳 Failed to find node"
+
+echo "🤩 All dependencies found! "
+
+echo "🪚 Generating the radar files"
+for file in "${data_folder}"/*.csv; do
+    base_filename="${file##*/}"
+    revision_folder_name="${base_filename%.csv}"
+    echo "🪵 Generating radar files from $base_filename"
+    python3 csv_to_md.py "$file" "$radar_folder/$revision_folder_name" || fatal "😵‍💫 Failed to generate markdown files from $file to $radar_folder/$base_filename."
+done
+echo "🛖 Generated radar markdown files successfully"
+
+# Build the website
+
+echo "🧰 Building the website"
+
+cd $frontend_folder || fatal "Failed to find the frontend folder!"
+
+echo "🔧 First time setup of website frontend"
+npm install || fatal "Failed to run npm install for frontend"
+echo "🔨 Compiling files"
+npm run build || fatal "Failed to build frontend"
+echo "🔩 Generating json files files"
+node ./dist_scripts/scripts/generateJson.js || fatal "Failed to generate the rd.json file"
+echo "🛠 Creating static files"
+node ./dist_scripts/scripts/createStaticFiles.js || fatal "failed to create the static files"
+ 
+echo "🛌 Website built"
