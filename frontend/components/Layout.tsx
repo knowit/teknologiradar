08933932da4409b99373e047f6c5c02d66abb522
Alireza Balouch <gswape@gmail.com>
2022-10-06 13:31:31 +0200

    #29: Adding footer component, reorder imports, reformat.

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,18 +1,22 @@
-import { ReactNode } from "react";
-import { useRouter } from "next/router";
-import Header from "./Header";
-import styles from "./Layout.module.css";
+import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import Header from './Header';
+import styles from './Layout.module.css';
+import FooterComponent from './FooterComponent';
 
 interface Props {
   children: ReactNode;
 }
+
 const Layout = ({ children }: Props) => {
   const { pathname } = useRouter();
   const isRootPage = pathname === '/';
+
   return (
     <div className={styles.main}>
-      <Header forRoot={isRootPage}/>
+      <Header forRoot={isRootPage} />
       <main>{children}</main>
+      <FooterComponent />
     </div>
   );
 };
