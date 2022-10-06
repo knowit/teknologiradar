1c4bc861c8b3cdff6ee003e548d4ad867a69f116
Alireza Balouch <gswape@gmail.com>
2022-10-06 20:46:41 +0200

    #29: adding URL's

--- frontend/components/FooterComponent/links.ts
+++ frontend/components/FooterComponent/links.ts
@@ -5,14 +5,14 @@ export interface LinkType {
 }
 
 export const learnMoreLinks: LinkType[] = [
-  { id: 1, title: 'Kontakt oss', url: '' },
-  { id: 2, title: 'Knowit blogg', url: '' },
+  { id: 1, title: 'Kontakt oss', url: 'https://www.knowit.no/kontakt/' },
+  { id: 2, title: 'Knowit blogg', url: 'https://blogg.knowit.no/' },
   { id: 3, title: 'Våre tjenester', url: '' },
 ];
 
 export const otherLanguages: LinkType[] = [
-  { id: 1, title: 'NO - Norge', url: '' },
-  { id: 2, title: 'EN - English', url: '' },
-  { id: 3, title: 'FI - Soumi', url: '' },
-  { id: 4, title: 'SE - Sverige', url: '' },
+  { id: 1, title: 'NO - Norge', url: 'https://www.knowit.no/' },
+  { id: 2, title: 'EN - English', url: 'https://www.knowit.eu/' },
+  { id: 3, title: 'FI - Soumi', url: 'https://www.knowit.fi/' },
+  { id: 4, title: 'SE - Sverige', url: 'https://www.knowit.se/' },
 ];
