5a128628789fe677e36ca36a545a125d0ab7af09
Alireza Balouch <gswape@gmail.com>
2022-10-06 13:32:00 +0200

    #29: Footer component

---
+++ frontend/components/FooterComponent/index.tsx
@@ -0,0 +1,34 @@
+import footerStyle from './Footer.module.css';
+import type { LinkType } from './links';
+import { learnMoreLinks, otherLanguages } from './links';
+
+export default function FooterComponent() {
+  function renderLinks(links: LinkType[]) {
+    if (!links || links.length <= 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <a href={link.url}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <footer className={footerStyle.footer}>
+      <div>
+        <strong>Text</strong>
+        {renderLinks(learnMoreLinks)}
+      </div>
+      <div>
+        <strong>Text</strong>
+        {renderLinks(otherLanguages)}
+      </div>
+      <div>del 3</div>
+    </footer>
+  );
+}
