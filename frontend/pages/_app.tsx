1f06b222eb3cf5f5932dafdfc0bbeee6150e0368
Alireza Balouch <gswape@gmail.com>
2022-10-10 20:24:47 +0200

    #29: format

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import { appWithTranslation } from "next-i18next";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
