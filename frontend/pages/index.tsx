a5a1b0e0c15ef8d70aca6f981328a11d4ef8ec07
Alireza Balouch <gswape@gmail.com>
2022-10-10 20:24:35 +0200

    #29: adding links in the list

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,21 @@
-import type { GetStaticProps, NextPage } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import styles from "../styles/Home.module.css";
-import categories from "../data/categories";
-import CategorySelector from "../components/CategorySelector";
-import { useTranslation } from "next-i18next";
-import Head from "next/head";
+import type { GetStaticProps, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import styles from '../styles/Home.module.css';
+import categories from '../data/categories';
+import CategorySelector from '../components/CategorySelector';
+import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const { t } = useTranslation("home");
+  const { t } = useTranslation('home');
   return (
     <>
       <Head>
-        <title>{t("pageTitle")}</title>
+        <title>{t('pageTitle')}</title>
       </Head>
       <div className={styles.container}>
         <div className={styles.descriptionWrapper}>
-          <p className={styles.description}>{t("introduction")}</p>
+          <p className={styles.description}>{t('introduction')}</p>
         </div>
 
         <div className={styles.categoryWrapper}>
@@ -27,9 +27,7 @@ const Home: NextPage = () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const translations = locale
-    ? await serverSideTranslations(locale, ["common", "home"])
-    : {};
+  const translations = locale ? await serverSideTranslations(locale, ['common', 'home', 'links']) : {};
   return {
     props: {
       ...translations,
